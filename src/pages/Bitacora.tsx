4ae941331fd857a4bfa5213bc3d4531258e30025
aorastreo <aoloschiles@gmail.com>
2026-06-20 18:19:33 -0600

    fix: logo por remitente (origen) en boleta/bitacora inter-franquicia

--- src/pages/Bitacora.tsx
+++ src/pages/Bitacora.tsx
@@ -83,7 +83,7 @@ export default function Bitacora() {
               <img
                 src={isGanga(originName) ? "/logo-ganga.jpg" : "/logo.jpg"}
                 alt={isGanga(originName) ? "Ganga Santa Rosa" : "American Outlet"}
-                ...
+
                   {isGanga(originName) ? "Ganga Santa Rosa" : "American Outlet"}
                 className="w-16 h-16 object-contain rounded-lg"
               />
